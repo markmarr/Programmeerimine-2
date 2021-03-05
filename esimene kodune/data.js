8a2ee9456aff756117dcf164629862d56cdecdc1
markmarr <60479633+markmarr@users.noreply.github.com>
2021-03-05 16:00:18 +0200

    Create data.js

---
+++ esimene kodune/data.js
@@ -0,0 +1,39 @@
+courseNames = [
+    "Käsitöötehnoloogiad ja disain 2",
+    "Käsitöötehnoloogiad ja disain 3",
+    "Tervisejuht 2"
+]
+
+
+weeks = [
+    {
+        id: 1,
+        lessons: [
+            {
+                id: 1,
+                courseId: 1,
+                name: "Vanatehnika restaureerimine",
+                comment: "Kontaktõpe individuaalse graafiku järgi",
+                startTime: new Date("2021-02-25T10:00+02:00"),
+                duration: new Date("0-0-0T07:30+02:00"),
+            },
+            {
+                id: 2,
+                courseId: 2,
+                name: "Meistritöökoja praktika 2",
+                comment: "Kontaktõpe individuaalse graafiku järgi",
+                startTime: new Date("2021-02-25T10:00+02:00"),
+                duration: new Date("0-0-0T07:30+02:00"),
+            },
+            {
+                id: 3,
+                courseId: 3,
+                name: "Loomeettevõtlus",
+                comment: "Zoom",
+                startTime: new Date("2021-02-25T17:30+02:00"),
+                duration: new Date("0-0-0T02:30+02:00"),
+            }
+        ]
+    }
+];
+
