f59cf2afa19cccbb0601b1d46c7a2201c21fade0
markmarr <60479633+markmarr@users.noreply.github.com>
2021-03-08 20:47:06 +0200

    Add files via upload

---
+++ teine kodune/database.js
@@ -0,0 +1,81 @@
+database = {
+    weeks: [
+        {
+            id: 1,
+            lessons: [
+                {
+                    id: 1,
+                    courseId: 1,
+                    name: "Vanatehnika restaureerimine",
+                    teacher: "Laine Lemmik",
+                    room: 301,
+                    comment: "Kontaktõpe individuaalse graafiku järgi",
+                    startTime: new Date("2021-02-25T10:00+02:00"),
+                    duration: 7.3,
+                },
+                {
+                    id: 2,
+                    courseId: 2,
+                    name: "Meistritöökoja praktika 2",
+                    teacher: "Laine Lemmik",
+                    room: 301,
+                    comment: "Kontaktõpe individuaalse graafiku järgi",
+                    startTime: new Date("2021-02-25T10:00+02:00"),
+                    duration: 7.3,
+                },
+                {
+                    id: 3,
+                    courseId: 3,
+                    name: "Loomeettevõtlus",
+                    teacher: "Laine Lemmik",
+                    room: 301,
+                    comment: "Zoom",
+                    startTime: new Date("2021-02-25T17:30+02:00"),
+                    duration: 2.5,
+                }
+            ]
+        },
+        {
+            id: 2,
+            lessons: [
+                {
+                    id: 1,
+                    courseId: 1,
+                    name: "Vanatehnika restaureerimine",
+                    teacher: "Laine Lemmik",
+                    room: 301,
+                    comment: "Kontaktõpe individuaalse graafiku järgi",
+                    startTime: new Date("2021-02-25T10:00+02:00"),
+                    duration: 7.3,
+                },
+                {
+                    id: 2,
+                    courseId: 2,
+                    name: "Meistritöökoja praktika 2",
+                    teacher: "Laine Lemmik",
+                    room: 301,
+                    comment: "Kontaktõpe individuaalse graafiku järgi",
+                    startTime: new Date("2021-02-25T10:00+02:00"),
+                    duration: 7.3,
+                },
+                {
+                    id: 3,
+                    courseId: 3,
+                    name: "Loomeettevõtlus",
+                    teacher: "Laine Lemmik",
+                    room: 301,
+                    comment: "Zoom",
+                    startTime: new Date("2021-02-25T17:30+02:00"),
+                    duration: 2.5,
+                }
+            ]
+        }
+    ],
+    courseNames: [
+        "Käsitöötehnoloogiad ja disain 2",
+        "Käsitöötehnoloogiad ja disain 3",
+        "Tervisejuht 2"
+    ]
+};
+
+module.exports = database;
